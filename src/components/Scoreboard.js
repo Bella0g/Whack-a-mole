002a8ff0805f4d23d8b7caf3e6523bcb3a71d3a0
DaniellaIsaksson <Daniellas.isaksson@gmail.com>
2024-09-26 14:16:56 +0200

    Added Reaction scoreBoard

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -11,8 +11,9 @@ const Scoreboard = ({ returnToLogin }) => {
       .get("http://localhost:2000/api/users")
       .then((response) => {
         // Sort users by points in descending order
-        const sortedUsers = response.data.sort((a, b) => b.points - a.points);
-        setUsers(sortedUsers);
+        const sortedUsers = [...response.data].sort((a, b) => b.points - a.points);
+        const sortedReactions =[...response.data].sort((a, b)=> a.bestReactionTime - b.bestReactionTime);
+        setUsers(sortedUsers, sortedReactions);
       })
       .catch((error) => {
         console.error("Cant fetch users", error);
@@ -21,13 +22,8 @@ const Scoreboard = ({ returnToLogin }) => {
   }, []);
 
   return (
-    <div className="relative bg-white shadow-md rounded-lg p-6 w-full max-w-3xl">
-      <button
-        className="absolute top-4 right-4 bg-purple-700 text-white rounded-full px-3 py-1 hover:bg-red-700"
-        onClick={returnToLogin}
-      >
-        X
-      </button>
+    <div className="relative bg-white shadow-md rounded-lg p-6 w-full max-w-3xl" style={{display:'flex', justifyContent:'space-around'}}>
+<section>
       <h2 className="text-2xl font-bold mb-4 text-blue-600">High score</h2>
       <table className="table-auto w-full justify-center">
         <thead>
@@ -47,6 +43,34 @@ const Scoreboard = ({ returnToLogin }) => {
           ))}
         </tbody>
       </table>
+      </section>
+      <section>
+      <button
+        className="absolute top-4 right-4 bg-purple-700 text-white rounded-full px-3 py-1 hover:bg-red-700"
+        onClick={returnToLogin}
+      >
+        X
+      </button>
+      <h2 className="text-2xl font-bold mb-4 text-blue-600">Fastest Reaction</h2>
+      <table className="table-auto w-full justify-center">
+        <thead>
+          <tr className="bg-gray-200 text-left">
+            <th className="p-4">Place</th>
+            <th className="p-4">Player</th>
+            <th className="p-4">Reaction Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={user._id} className="border-b">
+              <td className="p-4">{index + 1}</td>
+              <td className="p-4">{user.playername}</td>
+              <td className="p-4">{user.bestReactionTime} ms</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </section>
     </div>
   );
 };
